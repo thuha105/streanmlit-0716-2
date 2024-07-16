2db16944e6441905b574b4f8ed7c7de579a7f4b9
thuha105 <thuha051093@gmail.com>
2024-07-16 17:42:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
 
-# Title of the app
-st.title("Simple Streamlit App")
-
-# Slider widget
-value = st.slider("Select a number", 0, 100, 50)
-
-# Display the selected value
-st.write("Selected number:", value)
-
+st.title('Hello, Streamlit!')
+st.write('This is a simple Streamlit app.')
